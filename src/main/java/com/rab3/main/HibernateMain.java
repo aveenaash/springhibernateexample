7d765cfe451a50a32bee50398e4e538b4f213ac9
Abhinash Bastola <abastola@urgent.ly>
2019-09-13 16:10:01 -0400

    spring hibernate example

---
+++ src/main/java/com/rab3/main/HibernateMain.java
@@ -0,0 +1,39 @@
+package com.rab3.main;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.rab3.entities.CustomerEntity;
+import com.rab3.repositories.CustomerRepository;
+
+public class HibernateMain {
+
+	public static void main(String[] args) {
+//		CustomerEntity cust = new CustomerEntity();
+//		cust.setName("abinash");
+//		cust.setEmail("abinash@gmail.com");
+//		cust.setPhone("1342412341");
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+		CustomerRepository customerRepository = (CustomerRepository) ctx.getBean("customerRepository");
+//		customerRepository.save(cust);
+		
+		System.out.println("**************************");
+		List<CustomerEntity> customers = customerRepository.getCustomerByName("abinash");
+		for(CustomerEntity c : customers) {
+			System.out.println(c.toString());
+		}
+		
+		System.out.println();
+		System.out.println("**************************");
+		
+		
+		CustomerEntity customer1 = customerRepository.getCustomerById(4l);
+		System.out.println(customer1.toString());
+		
+		
+	}
+
+}
